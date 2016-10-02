3d8d51f8b02de79d5b8251e0ec1d79e4cc63ec15
Kimserey Lam <kimserey.lam@gmail.com>
2016-10-02 21:49:28 +0100

    First commit

---
+++ CanvasTest/CanvasTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CanvasTest
+{
+	public class TestView : Image
+	{
+		public static readonly BindableProperty SelectionWidthProperty =
+			BindableProperty.Create(
+				propertyName: nameof(SelectionWidth),
+			  	returnType: typeof(int),
+			  	declaringType: typeof(TestView),
+			  	defaultValue: 0);
+
+		public int SelectionWidth
+		{
+			get { return (int)GetValue(SelectionWidthProperty); }
+			set { SetValue(SelectionWidthProperty, value); }
+		}
+
+		public static readonly BindableProperty SelectionHeightProperty =
+			BindableProperty.Create(
+				propertyName: nameof(SelectionHeight),
+			  	returnType: typeof(int),
+			  	declaringType: typeof(TestView),
+			  	defaultValue: 0);
+
+		public int SelectionHeight
+		{
+			get { return (int)GetValue(SelectionHeightProperty); }
+			set { SetValue(SelectionHeightProperty, value); }
+		}
+	}
+
+	public class App : Application
+	{
+		public App()
+		{
+			var layout = new AbsoluteLayout();
+			var view = new TestView { HeightRequest = 100, WidthRequest = 100 };
+			var widthSlider = new Slider();
+			var heightSlider = new Slider();
+
+			var sliders = new StackLayout
+			{
+				Spacing = 0,
+				Children = {
+					widthSlider,
+					heightSlider
+				}
+			};
+
+			widthSlider.ValueChanged += (sender, e) => {
+				view.SelectionWidth = (int)e.NewValue;
+			};
+			heightSlider.ValueChanged += (sender, e) => { 
+				view.SelectionHeight = (int)e.NewValue;
+			};
+
+			layout.Children.Add(view, new Rectangle(0, 0, 1, .8), AbsoluteLayoutFlags.All);
+			layout.Children.Add(sliders, new Rectangle(0, 1, 1, .2), AbsoluteLayoutFlags.All);
+
+			var content =
+				new ContentPage
+				{
+					Title = "Canvas test",
+					Content = layout
+				};
+
+			MainPage = new NavigationPage(content);
+		}
+	}
+}
---
+++ Droid/TestViewRenderer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xamarin.Forms;
+using Android.Views;
+using CanvasTest;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Graphics.Drawables;
+using Android.Content;
+using Android.Graphics.Drawables.Shapes;
+using CanvasTest.Droid;
+
+[assembly: ExportRenderer(typeof(TestView), typeof(TestViewRenderer))]
+namespace CanvasTest
+{
+	[Preserve(AllMembers = true)]
+	public class TestViewRenderer: ImageRenderer
+	{
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == TestView.SelectionWidthProperty.PropertyName
+			    || e.PropertyName == TestView.SelectionHeightProperty.PropertyName)
+			{
+				this.Invalidate();
+			}
+		}
+
+		protected override bool DrawChild(Canvas canvas, Android.Views.View child, long drawingTime)
+		{
+			var selectionWidth = ((TestView)this.Element).SelectionWidth;
+			var selectionHeight = ((TestView)this.Element).SelectionHeight;
+
+			var bitmap = BitmapFactory.DecodeResource(this.Context.Resources, Droid.Resource.Drawable.icon);
+
+			var path = new Path();
+			path.AddRect(
+				new RectF(
+					(this.Width / 2) - (selectionWidth / 2), 
+					(this.Height / 2) - (selectionHeight / 2),
+					(this.Width / 2) + (selectionWidth / 2),
+					(this.Height / 2) + (selectionHeight / 2)), 
+				Path.Direction.Ccw);
+
+			var paint = new Paint();
+			paint.StrokeWidth = 5;
+			paint.SetStyle(Paint.Style.Stroke);
+			paint.Color = Android.Graphics.Color.Blue;
+
+			canvas.DrawBitmap(bitmap, null, new Rect(0, 0, this.Width, this.Height), paint);
+			canvas.DrawPath(path, paint);
+			
+			path.Dispose();
+			paint.Dispose();
+
+			return base.DrawChild(canvas, child, drawingTime);
+		}
+	}
+}
